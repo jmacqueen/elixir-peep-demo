804b7531de803c12afa29bfb001713598216f14f
Jonathan MacQueen <jmacqueen@echo360.com>
2016-08-24 14:44:51 -0400

    more heroku prep

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "tranquil-earth-10786.herokuapp.com", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
