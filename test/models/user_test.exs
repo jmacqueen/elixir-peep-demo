91509350f2513060e6ad20f79317e86dac2cca56
Jonathan MacQueen <jmacqueen@echo360.com>
2016-08-25 15:14:06 -0400

    add auth to api endpoint

---
+++ test/models/user_test.exs
@@ -0,0 +1,40 @@
+defmodule Peepchat.UserTest do
+  use Peepchat.ModelCase
+
+  alias Peepchat.User
+
+  @valid_attrs %{email: "me@home.org",
+    password: "abcd1234",
+    password_confirmation: "abcd1234"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+
+  test "mis-matched password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "me@example.org",
+      password: "1234abcd",
+      password_confirmation: "abcd1234"})
+    refute changeset.valid?
+  end
+
+  test "missing password_confirmation doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1lh2bj1rjbk2"})
+    refute changeset.valid?
+  end
+
+  test "short password doesn't work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "1lh2d",
+      password_confirmation: "1lh2d"})
+    refute changeset.valid?
+  end
+end
